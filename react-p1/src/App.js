70faee1f09b3184ab118e697808d3546140d7dbc
arthurvguide <arthurvguideuk@gmail.com>
2022-09-02 11:32:37 +0000

    Few react features

--- react-p1/src/App.js
+++ react-p1/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Arthur"
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
-      <h1>Hi React</h1>
-      <p> My first app</p>
+      <h1>Hello World</h1>
+      <p>My name is {newName}, this is my first app</p>
+      <p> Sum: {sum(1,4)}</p>
     </div>
   );
 }
