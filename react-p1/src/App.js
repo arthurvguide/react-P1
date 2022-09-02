702534d278df6e36481b30903a1ec63fbffa384e
arthurvguide <arthurvguideuk@gmail.com>
2022-09-02 10:49:47 +0000

    Initial commit react-p1

---
+++ react-p1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi React</h1>
+      <p> My first app</p>
+    </div>
+  );
+}
+
+export default App;
