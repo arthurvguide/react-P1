c1a4833923ed3e9b273f0b814d603d6cff9a2270
arthurvguide <arthurvguideuk@gmail.com>
2022-09-06 10:36:15 +0000

    Props

--- react-p1/src/components/Item.js
+++ react-p1/src/components/Item.js
@@ -1,9 +1,21 @@
-function Item(props) {
+import PropTypes from "prop-types"
+
+function Item({brand, release}) {
     return (
         <>
-            <li>{props.brand}</li>
+            <li>{brand} - {release} </li>
         </>
     )
+}
+
+Item.propTypes = {
+    brand: PropTypes.string.isRequired,
+    release: PropTypes.number,
+}
+
+Item.defaultProps = {
+    brand: "none",
+    release: 0
 }
 
 export default Item
--- react-p1/src/components/List.js
+++ react-p1/src/components/List.js
@@ -5,8 +5,9 @@ function List() {
         <>
             <h1>Testing Fragments  -  LIST</h1>
             <ul>
-                <Item brand = "Nike"/>
-                <Item brand = "Adidas"/>
+                <Item brand="Nike" release ={1988}/>
+                <Item brand="Adidas" release ={1982}/>
+                <Item/>
             </ul>
         </>
     )
